4bf8dafa5f6a45967eea064a418972a62991ee7d
siddharth ajith <siddharthajith1717@gmail.com>
2023-12-18 23:51:45 -0800

    Initial commit

---
+++ schoolmanagementproject/src/main/java/com/siddharth/schoolmanagementproject/SchoolmanagementprojectApplication.java
@@ -0,0 +1,13 @@
+package com.siddharth.schoolmanagementproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolmanagementprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolmanagementprojectApplication.class, args);
+	}
+
+}
